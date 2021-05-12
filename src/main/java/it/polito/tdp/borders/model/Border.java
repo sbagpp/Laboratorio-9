001d0675f145305c42dbb8ba001e248ca8ce7996
sbagpp <sabagiuseppe2000@gmail.com>
2021-05-12 08:05:38 +0200

    creazione classi Border e Country

--- src/main/java/it/polito/tdp/borders/model/Border.java
+++ src/main/java/it/polito/tdp/borders/model/Border.java
@@ -1,5 +1,19 @@
 package it.polito.tdp.borders.model;
 
 public class Border {
-
+	private Country c1;
+	private Country c2;
+	public Border(Country c1, Country c2) {
+		super();
+		this.c1 = c1;
+		this.c2 = c2;
+	}
+	public Country getC1() {
+		return c1;
+	}
+	public Country getC2() {
+		return c2;
+	}
+	
+	
 }
--- src/main/java/it/polito/tdp/borders/model/Country.java
+++ src/main/java/it/polito/tdp/borders/model/Country.java
@@ -1,5 +1,57 @@
 package it.polito.tdp.borders.model;
 
 public class Country {
-
+	private int ccode;
+	private String stateAbb;
+	private String StateNme;
+	public Country(int ccode, String stateAbb, String stateNme) {
+		super();
+		this.ccode = ccode;
+		this.stateAbb = stateAbb;
+		StateNme = stateNme;
+	}
+	public int getCcode() {
+		return ccode;
+	}
+	public void setCcode(int ccode) {
+		this.ccode = ccode;
+	}
+	public String getStateAbb() {
+		return stateAbb;
+	}
+	public void setStateAbb(String stateAbb) {
+		this.stateAbb = stateAbb;
+	}
+	public String getStateNme() {
+		return StateNme;
+	}
+	public void setStateNme(String stateNme) {
+		StateNme = stateNme;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ccode;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Country other = (Country) obj;
+		if (ccode != other.ccode)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return  StateNme ;
+	}
+	
+	
 }
